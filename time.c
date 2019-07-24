5cdaee4823c1b346cb94b3c6e899831b31cce8c4
Lufuno Israel KHUVHE <lkhuvhe@c5r6s2.wethinkcode.co.za>
2019-07-24 10:45:10 +0200

    files

--- time.c
+++ time.c
@@ -16,13 +16,52 @@ int main(void)
 	//time(&s);
 	//DIR *dir = opendir(".");
 	//struct dirent *read = readdir(dir);
-	if (stat(".", &stats) != -1)
+	if (stat("..", &stats) != -1)
 	{
 		// permisions
 		if (S_ISDIR(stats.st_mode))
 			ft_putchar('d');
 		else
 			ft_putchar('-');
+		if (stats.st_mode & S_IRUSR)
+			ft_putchar('r');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IWUSR)
+			ft_putchar('w');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IXUSR)
+			ft_putchar('x');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IRGRP)
+			ft_putchar('r');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IWGRP)
+			ft_putchar('w');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IXGRP)
+			ft_putchar('x');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IROTH)
+			ft_putchar('r');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IWOTH)
+			ft_putchar('w');
+		else
+			ft_putchar('-');
+		if (stats.st_mode & S_IXOTH)
+			ft_putchar('x');
+		else
+			ft_putchar('-');
+			
+			
+		ft_putchar(' ');
 		// hard links
 		ft_putnbr(stats.st_nlink);
 		ft_putchar(' ');
