b67671fc7d2fde85a0bbfb4c084da323243a09ef
Lufuno Israel KHUVHE <lkhuvhe@c5r6s2.wethinkcode.co.za>
2019-09-19 17:01:37 +0200

    norm fixed

--- creat_lst_dir.c
+++ creat_lst_dir.c
@@ -6,19 +6,28 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 18:14:28 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/09/18 18:42:45 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/09/19 15:41:05 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static void	error(char *directory)
+static void		error(char *directory)
 {
 	ft_putstr("ft_ls: ");
 	perror(directory);
 }
 
-t_list		*creat_lst_dir(char *directory, char *option)
+static t_list	*ret_close(DIR *current_dir, t_list *ls_list)
+{
+	t_list *sort_ls;
+
+	sort_ls = lst_sort_ascii(ls_list);
+	closedir(current_dir);
+	return (sort_ls);
+}
+
+t_list			*creat_lst_dir(char *directory, char *option)
 {
 	t_list	*ls_list;
 	t_list	*sort_ls;
@@ -30,22 +39,19 @@ t_list		*creat_lst_dir(char *directory, char *option)
 	if ((current_dir = opendir(directory)))
 	{
 		if (is_option('a', option) == 0)
-		{
 			while ((r = readdir(current_dir)))
 			{
 				len = ft_strlen(r->d_name) + 1;
 				if (r->d_name[0] != '.')
 					ft_lstadd(&ls_list, ft_lstnew(r->d_name, len));
 			}
-		}
 		else
 			while ((r = readdir(current_dir)))
 				ft_lstadd(&ls_list, ft_lstnew(r->d_name, \
 							ft_strlen(r->d_name) + 1));
-		sort_ls = lst_sort_ascii(ls_list);
-		closedir(current_dir);
+		sort_ls = ret_close(current_dir, ls_list);
 		return (sort_ls);
 	}
-	error(directory);
-	return 0;
+	perror(directory);
+	return (0);
 }
